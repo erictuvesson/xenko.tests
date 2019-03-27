b93fccf3120108e5cee9b5a1f78ba3cde6a8f9a5
Eric Tuvesson <eric.tuvesson@gmail.com>
2019-03-27 22:14:19 +0100

    Use SpriteBatch with fonts

--- src/XenkoTests/Game1.cs
+++ src/XenkoTests/Game1.cs
@@ -5,36 +5,32 @@
     using Xenko.Core.Mathematics;
     using Xenko.Engine;
     using Xenko.Games;
-    using Xenko.Graphics;
     using Xenko.Rendering;
-    using Xenko.Rendering.Compositing;
     using Xenko.Rendering.Materials;
     using Xenko.Rendering.Materials.ComputeColors;
     using Xenko.Rendering.ProceduralModels;
-    using Xenko.UI;
-    using Xenko.UI.Controls;
-    using Xenko.UI.Panels;
 
-    public class Game1 : Game
+    public class Game1 : GameCore
     {
-        private SpriteFont arial16;
         private Entity cubeEntity;
 
-        protected async override Task LoadContent()
+        protected override Task<Scene> BuildScene()
         {
-            await base.LoadContent();
-
-            Window.AllowUserResizing = true;
-
-            // Instantiate a scene with a single entity and model component
             var scene = new Scene();
 
-            arial16 = Content.Load<SpriteFont>("Arial18");
+            scene.Entities.Add(cubeEntity = GetCube());
+            scene.Entities.Add(GetLight());
 
-            scene.Entities.Add(GetUIEntity(arial16, true, new Vector3(0, 0, 4)));
+            var cameraEntity = new Entity { new CameraComponent { Slot = Services.GetSafeServiceAs<SceneSystem>().GraphicsCompositor.Cameras[0].ToSlotId() } };
+            cameraEntity.Transform.Position = new Vector3(0, 0, 5);
+            scene.Entities.Add(cameraEntity);
 
-            // Create a cube entity
-            cubeEntity = new Entity();
+            return Task.FromResult(scene);
+        }
+
+        protected Entity GetCube()
+        {
+            var cubeEntity = new Entity();
 
             // Create a procedural model with a diffuse material
             var model = new Model();
@@ -52,18 +48,11 @@
             var modelDescriptor = new ProceduralModelDescriptor(new CubeProceduralModel());
             modelDescriptor.GenerateModel(Services, model);
 
-            // Add the cube to the scene
-            scene.Entities.Add(cubeEntity);
+            return cubeEntity;
+        }
 
-            // Use this graphics compositor
-            SceneSystem.GraphicsCompositor = GraphicsCompositorHelper.CreateDefault(false, graphicsProfile: GraphicsProfile.Level_9_1);
-
-            // Create a camera entity and add it to the scene
-            var cameraEntity = new Entity { new CameraComponent { Slot = Services.GetSafeServiceAs<SceneSystem>().GraphicsCompositor.Cameras[0].ToSlotId() } };
-            cameraEntity.Transform.Position = new Vector3(0, 0, 5);
-            scene.Entities.Add(cameraEntity);
-
-            // Create a light
+        protected Entity GetLight()
+        {
             var lightEntity = new Entity()
             {
                 new LightComponent()
@@ -71,79 +60,25 @@
 
             lightEntity.Transform.Position = new Vector3(0, 0, 1);
             lightEntity.Transform.Rotation = Quaternion.RotationY(MathUtil.DegreesToRadians(45));
-            scene.Entities.Add(lightEntity);
 
-            // Create a scene instance
-            SceneSystem.SceneInstance = new SceneInstance(Services, scene);
+            return lightEntity;
         }
 
-        protected Entity GetUIEntity(SpriteFont font, bool fixedSize, Vector3 position)
+        protected override void Update(GameTime gameTime)
         {
-            // Create and initialize "Touch Screen to Start"
-            var touchStartLabel = new ContentDecorator
-            {
-                Content = new TextBlock
-                {
-                    Font = font,
-                    TextSize = 32,
-                    Text = (fixedSize) ? "Fixed Size UI" : "Regular UI",
-                    TextColor = Color.White
-                },
-                Padding = new Thickness(30, 20, 30, 25),
-                HorizontalAlignment = HorizontalAlignment.Center
-            };
+            base.Update(gameTime);
 
-            //touchStartLabel.SetPanelZIndex(1);
-
-            var grid = new Grid
-            {
-                BackgroundColor = (fixedSize) ? new Color(255, 0, 255) : new Color(255, 255, 0),
-                MaximumWidth = 100,
-                MaximumHeight = 100,
-                MinimumWidth = 100,
-                MinimumHeight = 100,
-                VerticalAlignment = VerticalAlignment.Center,
-                HorizontalAlignment = HorizontalAlignment.Center,
-            };
-
-            grid.RowDefinitions.Add(new StripDefinition(StripType.Auto));
-            grid.ColumnDefinitions.Add(new StripDefinition());
-            grid.LayerDefinitions.Add(new StripDefinition());
-
-            grid.Children.Add(touchStartLabel);
-
-            // Add the background
-            var background = new ImageElement { StretchType = StretchType.Fill };
-            background.SetPanelZIndex(-1);
-
-            var uiEntity = new Entity();
-
-            // Create a procedural model with a diffuse material
-            var uiComponent = new UIComponent
-            {
-                Page = new UIPage
-                {
-                    RootElement = new UniformGrid { Children = { background, grid } }
-                },
-                //IsBillboard = true,
-                IsFixedSize = fixedSize,
-                IsFullScreen = false,
-                Resolution = new Vector3(100, 100, 100), // Same size as the inner grid
-                Size = new Vector3(0.1f), // 10% of the vertical resolution
-            };
-            uiEntity.Add(uiComponent);
-
-            uiEntity.Transform.Position = position;
-
-            return uiEntity;
+            var time = (float)gameTime.Total.TotalSeconds;
+            cubeEntity.Transform.Rotation = Quaternion.RotationY(time) * Quaternion.RotationX(time * 0.5f);
         }
 
         protected override void Draw(GameTime gameTime)
         {
             base.Draw(gameTime);
 
-            var time = (float)gameTime.Total.TotalSeconds;
-            cubeEntity.Transform.Rotation = Quaternion.RotationY(time) * Quaternion.RotationX(time * 0.5f);
+            SpriteBatch.Begin(GraphicsContext);
+            SpriteBatch.DrawString(Arial18, "Hello World", new Vector2(50, 50), Color.White);
+            SpriteBatch.End();
         }
     }
 }
---
+++ src/XenkoTests/GameCore.cs
@@ -0,0 +1,30 @@
+﻿namespace XenkoTests
+{
+    using System.Threading.Tasks;
+    using Xenko.Engine;
+    using Xenko.Graphics;
+    using Xenko.Rendering.Compositing;
+
+    public abstract class GameCore : Game
+    {
+        protected SpriteBatch SpriteBatch { get; private set; }
+
+        protected SpriteFont Arial18 { get; private set; }
+
+        protected async override Task LoadContent()
+        {
+            await base.LoadContent();
+
+            Window.AllowUserResizing = true;
+
+            SpriteBatch = new SpriteBatch(GraphicsDevice);
+
+            Arial18 = Content.Load<SpriteFont>("DynamicFonts/Arial18");
+
+            SceneSystem.GraphicsCompositor = GraphicsCompositorHelper.CreateDefault(false, graphicsProfile: GraphicsProfile.Level_9_1);
+            SceneSystem.SceneInstance = new SceneInstance(Services, await BuildScene());
+        }
+
+        protected abstract Task<Scene> BuildScene();
+    }
+}
