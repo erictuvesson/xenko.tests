26bb8c698a5ad51cf25fa84a34f5ec28edf28618
Eric Tuvesson <eric.tuvesson@gmail.com>
2019-03-27 22:40:58 +0100

    Add TakeScreenshot method

--- src/XenkoTests/GameCore.cs
+++ src/XenkoTests/GameCore.cs
@@ -1,8 +1,12 @@
 ﻿namespace XenkoTests
 {
+    using System;
+    using System.IO;
     using System.Threading.Tasks;
     using Xenko.Engine;
+    using Xenko.Games;
     using Xenko.Graphics;
+    using Xenko.Input;
     using Xenko.Rendering.Compositing;
 
     public abstract class GameCore : Game
@@ -26,5 +30,39 @@
         }
 
         protected abstract Task<Scene> BuildScene();
+
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+#if DEBUG
+            if (Input.IsKeyDown(Keys.Escape))
+            {
+                Exit();
+            }
+#endif
+
+            if (Input.IsKeyReleased(Keys.F12))
+            {
+                TakeScreenshot();
+            }
+        }
+
+        protected void TakeScreenshot(string filename = null)
+        {
+            if (string.IsNullOrWhiteSpace(filename))
+            {
+                filename = $"screenshot-{DateTime.Now.ToString("yyyyMMddHHmmssffff")}.png";
+            }
+
+            var fullName = Path.Combine(Directory.GetCurrentDirectory(), filename);
+            using (var image = GraphicsDevice.Presenter.BackBuffer.GetDataAsImage(GraphicsContext.CommandList))
+            {
+                using (var resultFileStream = File.OpenWrite(fullName))
+                {
+                    image.Save(resultFileStream, ImageFileType.Png);
+                }
+            }
+        }
     }
 }
